5c9cf1c89ca0a64e6c23c7d4ad09b060205296bb
Dino <dineshnarayan19@gmail.com>
2023-09-20 14:50:04 +0530

    select songs using rofi and play them using mpc over mpd

---
+++ mpc-rofi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define your music directory
+MUSIC_DIR="/home/dino/Music"
+
+# Change to the music directory
+cd "$MUSIC_DIR" || exit
+
+# Use rofi to select music files and append them to the current playlist
+selected_files=$(find . -type f \( -name "*.mp3" -o -name "*.ogg" -o -name "*.wav" -o -name "*.flac" \) -printf "%P\n" | rofi -dmenu -i -multi-select -p "Select music files:")
+
+# Clear the playlist before adding the selected songs
+mpc clear
+
+# Check if files were selected
+if [ -n "$selected_files" ]; then
+  echo "$selected_files" | xargs -d '\n' -I{} mpc add "{}"
+  mpc play
+fi
+
