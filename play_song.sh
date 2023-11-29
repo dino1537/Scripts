fd14732b86502a618c14b17986c1f5dca7fc7095
Dino <dineshnarayan19@gmail.com>
2023-11-30 00:46:01 +0530

    made it much better and learned about IFS today and used it in this script to list all the songs

--- play_song.sh
+++ play_song.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-# Check if mplayer is installed
-if ! command -v mplayer &> /dev/null; then
-    echo "mplayer is not installed. Please install it to play songs."
+# Check if mpv is installed
+if ! command -v mpv &> /dev/null; then
+    echo "mpv is not installed. Please install it to play songs."
     exit 1
 fi
 
 # Set the path to your music folder
 music_folder="/home/dino/Music/"
 
-# Prompt the user for the song filename
-read -p "Enter the name of the song file (including the extension): " song_file
+# Get a list of all song files in the music folder and its subdirectories
+IFS=$'\n' song_files=( $(find "$music_folder" -type f) )
 
-# Check if the specified song file exists in the music folder
-if [ -f "$music_folder/$song_file" ]; then
-    # Play the specified song using mplayer
-    mplayer "$music_folder/$song_file"
-else
-    echo "The specified song file does not exist in the music folder."
-    exit 1
-fi
+# Prompt the user to select a song from the list
+echo "Please select a song:"
+select song_file in "${song_files[@]}"; do
+    # Check if the user has selected a valid option
+    if [[ -n $song_file ]]; then
+        # Play the selected song using mpv
+        mpv "$song_file"
+        break
+    else
+        echo "Invalid selection. Please try again."
+    fi
+done
 
 # Exit the script
 exit 0
-
